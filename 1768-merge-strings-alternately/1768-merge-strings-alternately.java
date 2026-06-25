f3600b556f3f3ac4823075fdbecba05ed034b8fa
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-06-25 23:24:09 +0530

    Time: 1 ms (95.94%), Space: 43 MB (77.83%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+            if (j < n) {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return sb.toString();
+    }
+}
