709272cb5555097bb976fe89f0422e220b79c819
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-07-19 10:20:46 +0530

    Time: 2 ms (82.09%), Space: 42.7 MB (81.18%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.java
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.java
@@ -1,17 +1,13 @@
 class Solution {
     public String complexNumberMultiply(String num1, String num2) {
-        int i=1;
-         String p="";
-          String q="";
-           String r="";
-            String s="";
+      
             int idx=num1.indexOf('+');
-             p=num1.substring(0,idx);
-             q=num1.substring(idx+1,num1.length()-1 );
+            String p=num1.substring(0,idx);
+             String q=num1.substring(idx+1,num1.length()-1 );
 
              idx=num2.indexOf('+');
-             r=num2.substring(0,idx);
-             s=num2.substring(idx+1,num2.length()-1);
+            String r=num2.substring(0,idx);
+            String s=num2.substring(idx+1,num2.length()-1);
              
         int real1=Integer.parseInt(p);
         int imag1=Integer.parseInt(q);
