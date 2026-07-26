60cbe531488dec045ec49ef03c69066c94aa394d
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-07-26 20:15:29 +0530

    Time: 0 ms (100%), Space: 46.5 MB (16.94%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,39 +1,50 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode oddEvenList(ListNode head) {
-        ListNode d1=new ListNode(-1);
-        ListNode d2=new ListNode(-1);
-        ListNode t1=d1;
-        ListNode t2=d2;
-        ListNode t=head;
-        int x=1;
-        while(t!=null)
-        {
-            if(x%2==1)
-            {
-                t1.next=t;
-                t1=t1.next;
-            }
-            else
-            {
-                t2.next=t;
-                t2=t2.next;
-            }
-            t=t.next;
-            x++;
-        }
-        t1.next=d2.next;
-        t2.next=null;
-        return d1.next;
 
+//class Solution {
+//    public ListNode oddEvenList(ListNode head) {
+//        ListNode d1=new ListNode(-1);
+//        ListNode d2=new ListNode(-1);
+//      ListNode t1=d1;
+//        ListNode t2=d2;
+//        ListNode t=head;
+//        int x=1;
+//        while(t!=null)
+//        {
+//            if(x%2==1)
+//            {
+//                t1.next=t;
+ //               t1=t1.next;
+ //           }
+ //          else
+ //           {
+ //               t2.next=t;
+ //               t2=t2.next;
+//          }
+//            t=t.next;
+//            x++;
+//        }
+//        t1.next=d2.next;
+//        t2.next=null;
+//        return d1.next//;
+
+//    }
+//
+//}
+class Solution {
+   public ListNode oddEvenList(ListNode head) {
+    if(head==null || head.next==null)
+    return head;
+    ListNode i=head;
+    ListNode j=head.next;
+    ListNode t=head.next;
+    while(j!=null && j.next!=null)
+    {
+        i.next=j.next;
+        i=i.next;
+
+        j.next=i.next;
+        j=j.next;
     }
+    i.next=t;
+    return head;
+   }
 }
