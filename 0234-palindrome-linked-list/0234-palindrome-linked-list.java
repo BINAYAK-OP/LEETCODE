841365a1d71c89666a23a6256600759a626646e5
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-07-27 21:19:51 +0530

    Time: 5 ms (43.14%), Space: 94.4 MB (67.77%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,24 +8,62 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//class Solution {
+//    public boolean isPalindrome(ListNode head) {
+//        ListNode temp=head;
+//        ArrayList<Integer> arr=new ArrayList<>();
+//        while(temp!=null)
+//        {
+//            arr.add(temp.val);
+//            temp=temp.next;
+//        }
+//        int i=0,j=arr.size()-1;
+//        while(i<=j)
+//        {
+//            int a=arr.get(i),b=arr.get(j);
+//            if(a!=b)
+//            return false;
+//            i++;
+ //           j--;
+//        }
+//        return true;
+//    }
+//}
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp=head;
-        ArrayList<Integer> arr=new ArrayList<>();
-        while(temp!=null)
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null)
         {
-            arr.add(temp.val);
-            temp=temp.next;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int i=0,j=arr.size()-1;
-        while(i<=j)
+        ListNode head2=slow.next;
+        slow.next=null;
+        head2=reverseList(head2);
+        ListNode i=head;
+        ListNode j=head2;
+        while(j!=null)
         {
-            int a=arr.get(i),b=arr.get(j);
-            if(a!=b)
+            if(i.val!=j.val)
             return false;
-            i++;
-            j--;
+            i=i.next;
+            j=j.next;
         }
         return true;
+    }
+     public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode fwd=null;
+         while(curr!=null)
+        {
+            fwd=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=fwd;
+        }
+        return prev;
     }
 }
