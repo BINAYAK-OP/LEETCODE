d50d83f4f360141cdddb40af40d25d13ddf64bda
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-07-30 21:12:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (49.55%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -8,30 +8,71 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//class Solution {
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        ArrayList<ListNode> arr=new ArrayList<>();
+//        ListNode temp=head;
+//        while(temp!=null)
+//        {
+//            arr.add(temp);
+//            temp=temp.next;
+//        }
+//        int i=left-1,j=right-1;
+//        while(i<j)
+//        {
+//            ListNode t1=arr.get(i);
+//            ListNode t2=arr.get(j);
+//            arr.set(i,t2);
+//            arr.set(j,t1);
+//            i++;
+//            j--;
+//        }
+//        for(i=0;i<arr.size()-1;i++)
+//        {
+//            arr.get(i).next=arr.get(i+1);
+//        }
+//        arr.get(arr.size()-1).next=null;
+//        return arr.get(0);
+//    }
+//}
+
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ArrayList<ListNode> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            arr.add(temp);
-            temp=temp.next;
-        }
-        int i=left-1,j=right-1;
-        while(i<j)
-        {
-            ListNode t1=arr.get(i);
-            ListNode t2=arr.get(j);
-            arr.set(i,t2);
-            arr.set(j,t1);
-            i++;
-            j--;
-        }
-        for(i=0;i<arr.size()-1;i++)
-        {
-            arr.get(i).next=arr.get(i+1);
-        }
-        arr.get(arr.size()-1).next=null;
-        return arr.get(0);
+   public ListNode reverseBetween(ListNode head, int left, int right) {
+    ListNode d=new ListNode(-1);
+    ListNode t=d;
+    d.next=head;
+    int i;
+    for(i=0;i<left-1;i++)
+    {
+        t=t.next;
     }
+    ListNode tail1=t;
+    ListNode head2=t.next;
+    for(i=0;i<right-left;i++)
+    {
+        t=t.next;
+    }
+    ListNode tail2=t.next;
+    ListNode head3=tail2.next;
+    tail1.next=null;
+    tail2.next=null;
+    t=reverse(head2);
+    tail1.next=t;
+    head2.next=head3;
+    return d.next;
+   }
+   public static ListNode reverse(ListNode head2)
+   {
+    ListNode curr=head2;
+    ListNode prev=null;
+    ListNode fwd=null;
+    while(curr!=null)
+    {
+        fwd=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=fwd;
+    }
+    return prev;
+   }
 }
