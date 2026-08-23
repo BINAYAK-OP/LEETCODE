b369a0d0c577bf22ceb18cbcae4fc3dd231512b1
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-08-23 11:58:21 +0530

    Time: 1 ms (96.91%), Space: 47.4 MB (40.44%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,25 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         while(head!=null && head.val==val)
+//         {
+//             head=head.next;
+//         }
+//          if(head==null)
+//         return null;
+//         ListNode prev=head;
+//         ListNode temp=head.next;
+//         while(temp!=null)
+//         {
+//             if(temp.val==val)
+//             {
+//                 prev.next=temp.next;
+//             }
+//             else
+//             {
+//                 prev=temp;
+//             }
+//             temp=temp.next;
+//         }
+//         return head;
+//     }
+// }
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         while(head!=null && head.val==val)
         {
             head=head.next;
         }
-         if(head==null)
+        if(head==null)
         return null;
-        ListNode prev=head;
-        ListNode temp=head.next;
-        while(temp!=null)
+        ListNode temp=head;
+        ListNode i;
+        while(temp!=null && temp.next!=null)
         {
-            if(temp.val==val)
+            if(temp.next.val==val)
             {
-                prev.next=temp.next;
-            }
-            else
-            {
-                prev=temp;
+                i=temp.next;
+                while(i!=null && i.val==val)
+                i=i.next;
+
+                temp.next=i;
             }
             temp=temp.next;
         }
