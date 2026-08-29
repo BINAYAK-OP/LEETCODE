a92b88370bb05d4542b1a6a475d5288931c1acc4
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-08-29 12:41:05 +0530

    Time: 2 ms (28.25%), Space: 42.7 MB (85.16%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -3,30 +3,50 @@ class MyStack {
     public MyStack() {
         
     }
-    
-    public void push(int x) {
+    public void push(int x)
+    {
+        // q ke front pe add karna
         q.add(x);
-    }
-    
-    public int pop() {
         int n=q.size();
         for(int i=1;i<=n-1;i++)
         {
             q.add(q.remove());
         }
+    }
+
+    public int pop()
+    {
         return q.remove();
     }
-    
-    public int top() {
-        int n=q.size();
-        for(int i=1;i<=n-1;i++)
-        {
-            q.add(q.remove());
-        }
-        int p= q.peek();
-        q.add(q.remove());
-        return p;
+
+    public int top()
+    {
+        return q.peek();
     }
+    
+    // public void push(int x) {
+    //     q.add(x);
+    // }
+    
+    // public int pop() {
+    //     int n=q.size();
+    //     for(int i=1;i<=n-1;i++)
+    //     {
+    //         q.add(q.remove());
+    //     }
+    //     return q.remove();
+    // }
+    
+    // public int top() {
+    //     int n=q.size();
+    //     for(int i=1;i<=n-1;i++)
+    //     {
+    //         q.add(q.remove());
+    //     }
+    //     int p= q.peek();
+    //     q.add(q.remove());
+    //     return p;
+    // }
     
     public boolean empty() {
         return (q.size()==0);
