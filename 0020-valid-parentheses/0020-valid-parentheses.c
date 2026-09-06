fe762f93f44ed40f8ededa2bbb30da28187edcf5
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-09-06 13:08:18 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,45 +1,21 @@
-void push(char stack[], int *top, char ch) {
-    (*top)++;
-    stack[*top] = ch;
-}
-
-char peek(char stack[], int top) {
-    return stack[top];
-}
 bool isValid(char* s) {
-    int n=strlen(s);
-    if(n%2==1)
-    return false;
-    char stack[n];
+    char stack[10000];
     int top=-1;
-    for(int i=0;i<n;i++)
+    for(int i=0;s[i]!='\0';i++)
     {
-        char ch=s[i];
-        if(ch=='(' || ch=='{' || ch=='[')
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+        stack[++top]=s[i];
+        else
         {
-        push(stack,&top,ch);
-        }
-        else 
-        {
-            
-            if(top == -1)
-                return false;
+            if(top==-1)
+            return false;
 
-            char t = peek(stack, top);
-
-            if(ch == '}' && t == '{') {
-                top--;
-            }
-            else if(ch == ']' && t == '[') {
-                top--;
-            }
-            else if(ch == ')' && t == '(') {
-                top--;
-            }
-            else {
-                return false;
+            char x=stack[top--];
+            if((x!='('&& s[i]==')') ||
+            (x!='{'&& s[i]=='}') ||
+            (x!='[' && s[i]==']'))
+            return false;
         }
-    }
-    }
+    } 
     return top==-1;
 }
