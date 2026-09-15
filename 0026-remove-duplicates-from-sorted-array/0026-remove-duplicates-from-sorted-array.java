afd8dde3d273f51d9c133db307a8eb65d3e444a0
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-09-15 23:00:03 +0530

    Time: 0 ms (100%), Space: 46.5 MB (91.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,41 @@
 
+//        class Solution {
+//      public int removeDuplicates(int[] nums) {
+
+//         if(nums.length == 0)
+//             return 0;
+
+//         int i = 0;
+
+//         for(int j = 1; j < nums.length; j++) {
+//             if(nums[j] != nums[i]) {
+//                 i++;
+//                 nums[i] = nums[j];
+//             }
+//         }
+
+//         return i + 1;
+//     }
+// }
+
+
        class Solution {
      public int removeDuplicates(int[] nums) {
 
         if(nums.length == 0)
             return 0;
 
-        int i = 0;
-
-        for(int j = 1; j < nums.length; j++) {
-            if(nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+            int i=0 ,j=i+1,k=0;
+            while(j<nums.length)
+            {
+                if(nums[j]!=nums[i])
+                {
+                    nums[k++]=nums[i];
+                    i=j;
+                }
+                j++;
             }
-        }
-
-        return i + 1;
-    }
-}
+            nums[k++]=nums[i];
+            return k;
+     }
+       }
