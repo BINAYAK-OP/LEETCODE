5e53fe51224e5ad9c554905a9e2cfc3ab1683609
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-09-17 20:02:45 +0530

    Time: 0 ms (100%), Space: 43.1 MB (70.23%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,30 +13,34 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         ArrayList <Integer> ans=new ArrayList<>();
-//         if(root==null)
-//         return;
-//         ans.add(preorderTraversal(root.left));
-//         ans.add(preordeTraversal(root.right));
-//         return ans;
-//     }
-//     public List<Integer> preorderTraversal(TreeNode root)
-// }
-
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        if(root == null)
-            return ans;
-
+    public void preorder(TreeNode root,ArrayList<Integer> ans) {
+        if(root==null)
+        return;
         ans.add(root.val);
-
-        ans.addAll(preorderTraversal(root.left));
-        ans.addAll(preorderTraversal(root.right));
-
-        return ans;
+       preorder(root.left,ans);
+        preorder(root.right,ans);
+    }
+    public List<Integer> preorderTraversal(TreeNode root)
+    {
+         ArrayList<Integer> ans = new ArrayList<>();
+         preorder(root,ans);
+         return ans;
     }
 }
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+
+//         if(root == null)
+//             return ans;
+
+//         ans.add(root.val);
+
+//         ans.addAll(preorderTraversal(root.left));
+//         ans.addAll(preorderTraversal(root.right));
+
+//         return ans;
+//     }
+// }
