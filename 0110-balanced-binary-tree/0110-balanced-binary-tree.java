12774bfba99f9a02d75016b651345d8c79bbea92
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-09-22 23:42:01 +0530

    Time: 0 ms (100%), Space: 45.6 MB (42.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,14 +14,18 @@
  * }
  */
 class Solution {
+    static boolean flag;
     public boolean isBalanced(TreeNode root) {
         if(root==null)
         return true;
-        int count1=level(root.left);
-        int count2=level(root.right);
-        if(Math.abs(count1-count2)>1)
-        return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        flag=true;
+        level(root);
+        // int count1=level(root.left);
+        // int count2=level(root.right);
+        // if(Math.abs(count1-count2)>1)
+        // return false;
+        // return isBalanced(root.left) && isBalanced(root.right);
+        return flag;
     }
     public int level(TreeNode root)
     {
@@ -29,6 +33,8 @@ class Solution {
         return 0;
         int leftlevel=level(root.left);
         int rightlevel=level(root.right);
+        if(Math.abs(leftlevel-rightlevel)>1)
+        flag=false;
         return 1+Math.max(leftlevel,rightlevel);
     }
 }
