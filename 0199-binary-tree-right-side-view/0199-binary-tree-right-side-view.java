bff87029f07007f7579ba7c07cd8a19e878efdb9
BINAYAK-OP <ishwarbinayak@gmail.com>
2026-09-23 20:24:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (50.59%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,27 +13,46 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans=new ArrayList<>();
+//         if(root==null)
+//         return ans;
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.add(root);
+//         while(q.size()>0)
+//         {
+//             int n=q.size();
+//            for(int i=0;i<n;i++)
+//            {
+//             TreeNode curr=q.remove();
+//             if(i==n-1)
+//             ans.add(curr.val);
+//             if(curr.left!=null)
+//             q.add(curr.left);
+//              if(curr.right!=null)
+//             q.add(curr.right);
+//            }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
+        view(root,0,ans);
+        return ans;
+    }
+    void view(TreeNode root,int level, List<Integer> ans)
+    {
         if(root==null)
-        return ans;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        while(q.size()>0)
-        {
-            int n=q.size();
-           for(int i=0;i<n;i++)
-           {
-            TreeNode curr=q.remove();
-            if(i==n-1)
-            ans.add(curr.val);
-            if(curr.left!=null)
-            q.add(curr.left);
-             if(curr.right!=null)
-            q.add(curr.right);
-           }
-        }
-        return ans;
+        return;
+        if(level>=ans.size())
+        ans.add(root.val);
+        else
+        ans.set(level,root.val);
+        view(root.left,level+1,ans);
+        view(root.right,level+1,ans);
     }
 }
